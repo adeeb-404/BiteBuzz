afc6c0c6e67be54a568d5e1a72e779ad165eb869
Adeeb <adeebm195@gmail.com>
2024-05-31 09:06:08 +0530

    Add styles for features

--- frontend/src/Components/homepage/Features.jsx
+++ frontend/src/Components/homepage/Features.jsx
@@ -1,21 +1,17 @@
 import Card from "../../Customs/Card";
-import { motion } from "framer-motion";
 
 function Features() {
   return (
-    <motion.section
-      id="features"
-      className=" cust-text md:pt-4 p-2"
-      animate={{ y: [500, 0], opacity: [0, 1] }}
-      transition={{ duration: 2, type: "spring" }}
-    >
-      <h1 className="text-5xl font-semibold p-2 md:p-16 md:pb-0 ">Features</h1>
-      <div className="flex flex-col md:flex-row items-center justify-center">
+    <section id="features" className=" cust-text md:pt-4 p-2">
+      <h1 className=" text-4xl md:text-5xl font-semibold p-2 md:p-16 md:pb-0 ">
+        Features
+      </h1>
+      <div className="flex flex-col md:flex-row items-center justify-center gap-2">
         <Card />
         <Card />
         <Card />
       </div>
-    </motion.section>
+    </section>
   );
 }
 
--- frontend/src/Customs/Card.jsx
+++ frontend/src/Customs/Card.jsx
@@ -1,16 +1,23 @@
 import { FcIdea } from "react-icons/fc";
+import { motion } from "framer-motion";
 
 function Card() {
   return (
-    <div className=" p-12 ">
-      <FcIdea className="h-12 w-12 mb-4" />
-      <h1 className="text-2xl font-bold mb-2">Convenient Preorder System: </h1>
+    <motion.div
+      className=" p-10 m-2 box-border rounded-xl border-2 dark:border-green-950 border-green-100"
+      whileHover={{ borderColor: "#a3e635" }}
+      transition={{ type: "spring" }}
+    >
+      <FcIdea className=" h-10 w-10 md:h-12 md:w-12 mb-2 md:mb-4 " />
+      <h1 className=" text-xl md:text-2xl font-bold mb-2">
+        Convenient Preorder System:
+      </h1>
       <p>
         Say goodbye to long queues! With our intuitive prebooking system, you
         can effortlessly select your meals ahead of time, ensuring you have more
         time to enjoy your breaks.
       </p>
-    </div>
+    </motion.div>
   );
 }
 
