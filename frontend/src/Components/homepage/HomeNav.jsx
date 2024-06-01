577012f1933231cc883a5728718de0948d062ac2
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-06-01 17:35:15 +0530

    Update navbar

--- frontend/src/Components/homepage/HomeNav.jsx
+++ frontend/src/Components/homepage/HomeNav.jsx
@@ -11,27 +11,27 @@ function tempHandler() {
 
 function HomeNav() {
   return (
-    <div className="cust-flex mx-3 mt-2 p-5 text-[1.3rem] bg-gray-100 rounded-md  font-bold sticky top-0">
-      <div className="cust-flex px-5 self-start">
+    <div className="navbar cust-flex md:mx-3 mt-2 h-[3rem] w-[25rem] text-[1rem] bg-gray-100 rounded-md gap-4 font-bold sticky top-0 md:text-[1.3rem] md:h-[3rem] md:w-full md:items-center">
+      <div className=" logo p-1 cust-flex md:px-5 self-start md:pt-0">
         <p className="px-2">logo</p>
-        <p>BiteBuzz</p>
+        <p className="hidden md:block">BiteBuzz</p>
       </div>
 
-      <div className="flex h-auto bg-white rounded-md">
-        <IoSearch className="px-2 size-9" />
+      <div className="flex h-8 bg-white rounded-md">
+        <IoSearch className="px-2 size-9 md:size-9" />
         <input
           placeholder="Search Canteen"
-          className="w-[25rem] h-7 px-2 py-4 rounded-md self font-normal"
+          className="w-[8rem] h-7 md:w-[25rem] md:h-7 px-2 py-4 rounded-md self font-normal"
           type="text"
         />
       </div>
 
-      <div className="cust-flex px-7 w-[30%]">
-        <MdOutlineShoppingCart className="mx-5 size-10" />
+      <div className="cust-flex md:px-7 md:w-[30%]">
+        <MdOutlineShoppingCart className="md:mx-5 size-10" />
         <p className="w-[50%]">welcome @user</p>
-        <IoSettingsSharp className="mx-2 size-7" />
+        <IoSettingsSharp className=" md:mx-2 md:size-7" />
         <Link onClick={tempHandler}>
-          <RiLogoutCircleLine className="mx-2 size-7" />
+          <RiLogoutCircleLine className="md:mx-2 size-7" />
         </Link>
       </div>
     </div>
--- frontend/src/Components/mainPage/Dashboard.jsx
+++ frontend/src/Components/mainPage/Dashboard.jsx
@@ -4,6 +4,7 @@ function Dashboard() {
   return (
     <div className='w-[70%]'>
         <h1> Dash board</h1>
+        <div className=''></div>
     </div>
   )
 }
