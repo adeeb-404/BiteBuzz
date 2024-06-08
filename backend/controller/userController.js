5b3e90c97ff12ae654a21001f949ad81cc95c644
Akshayraj N <akshayrajnk20@gmail.com>
2024-06-08 20:44:54 +0530

    update current orders in database

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,4 @@
-import users from "../model/userSchema.js";
+import User from "../model/userSchema.js";
 import OrdersSchema from "../model/baseOrderSchema.js";
 
 import Canteen from "../model/canteenSchema.js";
@@ -6,7 +6,7 @@ import { getStudentName, getStudentUSN } from "../utility/util.js";
 export async function userAuth(req, res) {
   console.log("In controller");
   const obj = req.body;
-  console.log(obj);
+  
 
   // Implement your authentication logic here
   try {
@@ -31,53 +31,80 @@ export async function userAuth(req, res) {
 
 export async function submitOrder(req, res) {
   let obj = req.body;
-  // console.log(obj);
-  const user = await users.findById(obj[0].userID);
-  const findCanteen = await Canteen.findById(obj[0].canteenID);
-  for (let i = 0; i < obj.length; i++) {
-    try {
-      // console.log(obj[i]);
-      const findCanteen = await Canteen.findById(obj[i].canteenID);
 
-      let findItem = await Canteen.findOne(
-        {
-          Name: findCanteen.Name,
-          menu: {
-            $elemMatch: {
-              dishName: obj[i].dishName,
+  try {
+    const user = await User.findById(obj[0].userID);
+    const findCanteen = await Canteen.findById(obj[0].canteenID);
+
+    for (let i = 0; i < obj.length; i++) {
+      try {
+        let findItem = await Canteen.findOne(
+          {
+            name: findCanteen.name,
+            menu: {
+              $elemMatch: {
+                dishName: obj[i].dishName,
+              },
             },
           },
-        },
-        { "menu.$": 1 }
-      );
-      // console.log("findItem");
-      findItem = findItem.toObject();
-      // console.log(findItem);
-      const currTime = new Date().getTime();
-      const time = findItem.menu[0].preparationTime * (60 * 1000);
-      const finalTime = new Date(currTime + time).toLocaleTimeString();
-      obj[i].expectedTime = finalTime;
-      // console.log(finalTime);
-      // const student=await getStudentNameAndUSN(obj[i].userID);
-      // obj[i].student=student;
-    } catch (err) {
-      console.error(err);
-      res.status(500);
-    }
-  }
-  const name = await getStudentName(user._id);
-  const usn = await getStudentUSN(user._id);
-  const responseBody = {
-    name,
-    usn,
-    orders: obj,
-  };
-  // console.log(obj);
+          { "menu.$": 1 }
+        );
 
-  res.json(responseBody);
-  // const updateCurrorders= await Canteen.findByIdAndUpdate({"_id":findCanteen._id},{"$push":{currOrders:obj}});
-  // console.log(updateCurrorders);
-  // console.log(obj);
+        if (findItem) {
+          findItem = findItem.toObject();
+          const currTime = new Date().getTime();
+          const time = findItem.menu[0].preparationTime * (60 * 1000);
+          const finalTime = new Date(currTime + time).toLocaleTimeString();
+          obj[i].expectedTime = finalTime;
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error finding item");
+        return;
+      }
+    }
+
+    const name = await getStudentName(user._id);
+    const usn = await getStudentUSN(user._id);
+
+    const ordersArray = obj.map(order => ({
+      userID: order.userID,
+      canteenID: order.canteenID,
+      itemName: order.dishName, // Assuming dishName corresponds to itemName
+      quantity: order.quantity,
+      price: order.price,
+      expectedTime: order.expectedTime
+    }));
+
+    const responseBody = {
+      name,
+      usn,
+      orders: ordersArray // This will be pushed as an array of orders
+    };
+
+    res.json(responseBody);
+
+    try {
+      await Canteen.findByIdAndUpdate(
+        { _id: findCanteen._id },
+        { $push: { currOrders: ordersArray } } // Push as an array of arrays of objects
+      );
+
+      await User.findByIdAndUpdate(
+        { _id: obj[0].userID },
+        { $push: { orders: ordersArray } } // Push as an array of arrays of objects
+      );
+    } catch (updateErr) {
+      console.error(updateErr);
+      res.status(500).send("Error updating orders");
+      return;
+    }
+
+    console.log(responseBody);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error processing order");
+  }
 }
 
 export async function dashboard(req, res) {
