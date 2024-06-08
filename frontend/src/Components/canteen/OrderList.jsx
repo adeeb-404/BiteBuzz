8d755ccc629d63833240e67ee0e742d10e2b3535
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-06-08 18:09:16 +0530

    Maodified Orderlist and canteen page

--- frontend/src/Components/canteen/OrderList.jsx
+++ frontend/src/Components/canteen/OrderList.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import orderItems from '../../assets/orderItems'
 
-function OrderList() {
+function OrderList(props) {
+    function handleCanteenClick(index){
+        console.log(index);
+        props.onclick = index;
+    }
   return (
     <div className='w-[20%] h-screen overflow-scroll'>
         <div className='bg-gray-100 h-fit flex gap-2 flex-col justify-center items-center'>
         <h1>Order List</h1>
         {orderItems.map( (element, index) => {
             return (
-                <div key={index} className='h-fit w-fit flex flex-col justify-center items-center bg-gray-400 rounded-lg'>
+                <div key={index} onClick={() => handleCanteenClick(index)} className='h-fit w-fit flex flex-col justify-center items-center bg-gray-400 rounded-lg'>
                     <img src={element.orderFood[0].foodImg} className='h-[5rem] w-[5rem] m-1 mt-3 rounded-md'></img>
                     <div className='flex flex-col justify-center mt-2'>
                         <div className='flex mx-3 w-fit h-fit justify-evenly'>
--- frontend/src/Pages/CanteenPage.jsx
+++ frontend/src/Pages/CanteenPage.jsx
@@ -3,9 +3,10 @@ import OrderList from "../Components/canteen/OrderList";
 import CanteenDashboard from "../Components/canteen/CanteenDashboard";
 
 function CanteenPage() {
+
   return (
   <div className="text-3xl flex h-screen pt-14 fixed w-full">
-    <OrderList />
+    <OrderList onclick={index}/>
     <CanteenDashboard />
   </div>
   )
