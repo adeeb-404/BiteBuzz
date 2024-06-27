39380b48b01cd6f6f510dd9f407cd820c816060b
Akshayraj N <akshayrajnk20@gmail.com>
2024-06-27 17:39:16 +0530

    add storage for displaying menu

--- backend/routes/canteenRoute.js
+++ backend/routes/canteenRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { canteenAuth } from "../controller/canteenController.js";
+import { canteenAuth, displayStorage } from "../controller/canteenController.js";
 const router = Router();
 
 router.post("/login", canteenAuth);
-
+router.get("/menu",displayStorage);
 export default router;
