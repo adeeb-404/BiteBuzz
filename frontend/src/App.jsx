8a8b0b28531613cab4c969b229890e35a79b8476
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-06-29 18:48:17 +0530

    Added Canteen History page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,6 +43,10 @@ const router = createBrowserRouter([
             path: "food",
             element: <FoodConfiguration />,
           },
+          {
+            path:"history",
+            element: <CanteenHistory />,
+          }
         ],
       },
       {
---
+++ frontend/src/Pages/CanteenHistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CanteenHistory() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CanteenHistory
