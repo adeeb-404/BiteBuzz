d646b134fff320b7c81121510719909d4782bde8
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-07-01 19:46:15 +0530

    Added dark mode to canteen main page

--- frontend/src/Components/canteen/orderBox.jsx
+++ frontend/src/Components/canteen/orderBox.jsx
@@ -2,8 +2,8 @@
 
 function OrderBox({ foods }) {
   return (
-    <div className="bg-green-200 w-[12rem] h-[12rem] flex items-center flex-col rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg border-2 border-green-950">
-      <h1 className="pb-1 text-[1.5rem] font-semibold text-green-900">
+    <div className="bg-green-200 dark:bg-[#282828] w-[12rem] h-[12rem] flex items-center flex-col rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg border-2 border-green-950 dark:border-green-900">
+      <h1 className="pb-1 text-[1.5rem] font-semibold text-green-900 dark:text-green-200">
         {foods.foodName}
       </h1>
       <img
@@ -11,10 +11,10 @@ function OrderBox({ foods }) {
         alt={foods.foodName}
         className="h-[5rem] w-[5rem] rounded-md"
       />
-      <div className="text-[1.2rem] text-green-700">
+      <div className="text-[1.2rem] text-green-700 dark:text-green-400">
         Quantity: {foods.quantity}
       </div>
-      <div className="flex items-center justify-around w-full text-green-900">
+      <div className="flex items-center justify-around w-full text-green-900 dark:text-green-200">
         <div className="text-[1.2rem]">₹ {foods.price}</div>
       </div>
     </div>
--- frontend/src/Pages/CanteenPage.jsx
+++ frontend/src/Pages/CanteenPage.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function CanteenPage() {
   const [index, setIndex] = useState(null);
   return (
-    <div className="flex h-screen fixed w-screen bg-green-50">
+    <div className="flex h-screen fixed w-screen bg-green-50 dark:bg-[#181818]">
       <OrderList setIndex={setIndex} />
       <CanteenDashboard index={index} />
     </div>
